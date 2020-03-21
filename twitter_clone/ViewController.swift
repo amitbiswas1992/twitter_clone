671e85259c60792ecfcaef69539238a1278ec4c4
Amit Biswas <biswas1992@hotmail.com>
2020-03-21 00:27:40 -0400

    Initial Commit

---
+++ twitter_clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitter_clone
+//
+//  Created by Amit Biswas on 3/21/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
