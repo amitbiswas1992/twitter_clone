3c97378dc20aae43636ee5af82a066b177bb61d2
Amit Biswas <biswas1992@hotmail.com>
2020-03-22 17:31:19 -0400

    Setup all VC

---
+++ twitter_clone/Controller/ExploreController.swift
@@ -0,0 +1,25 @@
+//
+//  ExploreController.swift
+//  twitter_clone
+//
+//  Created by Amit Biswas on 3/22/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ExploreController: UIViewController {
+
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    //MARK: - Helpers
+    
+
+}
---
+++ twitter_clone/Controller/MainTabController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  twitter_clone
+//
+//  Created by Amit Biswas on 3/22/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    //MARK: - Properties
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+    //MARK: - Helpers
+    
+    func configureViewControllers() {
+        
+    }
+    
+
+}
